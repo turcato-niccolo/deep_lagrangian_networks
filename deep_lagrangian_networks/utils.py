863b5902dd53f3ba2bfcd043725b1ac12f7303f0
Giulio <giulio.giacomuzzo@gmail.com>
2021-10-30 00:08:14 +0200

    Trained DeLaN on the complete panda dataset with lr 5e-4 and 10000 epochs. Results are poor.

--- deep_lagrangian_networks/utils.py
+++ deep_lagrangian_networks/utils.py
@@ -87,3 +87,51 @@ def load_dataset(n_characters=3, filename="data/DeLaN_Data.pickle"):
     return (train_labels, train_qp, train_qv, train_qa, train_p, train_pd, train_tau), \
            (test_labels, test_qp, test_qv, test_qa, test_p, test_pd, test_tau, test_m, test_c, test_g),\
            divider, dt_mean
+
+def load_dataset_panda(filename_train="data/FE_panda/FE_panda_pybul_tr.pkl", filename_test="data/FE_panda/FE_panda_pybul_test.pkl"):
+
+    data_train = pickle.load(open(filename_train, 'rb'))
+    data_test = pickle.load(open(filename_test, 'rb'))
+
+    n_dof = 7
+    qp_labels = ['q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6', 'q_7']
+    qv_labels = ['dq_1', 'dq_2', 'dq_3', 'dq_4', 'dq_5', 'dq_6', 'dq_7']
+    qa_labels = ['ddq_1', 'ddq_2', 'ddq_3', 'ddq_4', 'ddq_5', 'ddq_6', 'ddq_7']
+    tau_labels = ['tau_1', 'tau_2', 'tau_3', 'tau_4', 'tau_5', 'tau_6', 'tau_7']
+    train_qp, train_qv, train_qa, train_tau = data_train[qp_labels].to_numpy(), data_train[qv_labels].to_numpy(), data_train[qa_labels].to_numpy(), data_train[tau_labels].to_numpy()
+    test_qp, test_qv, test_qa, test_tau = data_test[qp_labels].to_numpy(), data_test[qv_labels].to_numpy(), data_test[qa_labels].to_numpy(), data_test[tau_labels].to_numpy()
+    return (train_qp, train_qv, train_qa, train_tau), \
+           (test_qp, test_qv, test_qa, test_tau)
+
+    # divider = [0, ]   # Contains idx between characters for plotting
+
+    # for i in range(len(data["labels"])):
+
+    #     if i in test_idx:
+    #         test_labels.append(data["labels"][i])
+    #         test_qp = np.vstack((test_qp, data["qp"][i]))
+    #         test_qv = np.vstack((test_qv, data["qv"][i]))
+    #         test_qa = np.vstack((test_qa, data["qa"][i]))
+    #         test_tau = np.vstack((test_tau, data["tau"][i]))
+
+    #         test_m = np.vstack((test_m, data["m"][i]))
+    #         test_c = np.vstack((test_c, data["c"][i]))
+    #         test_g = np.vstack((test_g, data["g"][i]))
+
+    #         test_p = np.vstack((test_p, data["p"][i]))
+    #         test_pd = np.vstack((test_pd, data["pdot"][i]))
+    #         divider.append(test_qp.shape[0])
+
+    #     else:
+    #         train_labels.append(data["labels"][i])
+    #         train_qp = np.vstack((train_qp, data["qp"][i]))
+    #         train_qv = np.vstack((train_qv, data["qv"][i]))
+    #         train_qa = np.vstack((train_qa, data["qa"][i]))
+    #         train_tau = np.vstack((train_tau, data["tau"][i]))
+
+    #         train_p = np.vstack((train_p, data["p"][i]))
+    #         train_pd = np.vstack((train_pd, data["pdot"][i]))
+
+    # return (train_labels, train_qp, train_qv, train_qa, train_p, train_pd, train_tau), \
+    #        (test_labels, test_qp, test_qv, test_qa, test_p, test_pd, test_tau, test_m, test_c, test_g),\
+    #        divider, dt_mean
