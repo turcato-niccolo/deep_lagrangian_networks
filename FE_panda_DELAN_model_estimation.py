e684b291e4cbfce7ec4f40237bbaf8800bbe60a9
turcato_niccolo <Lup3tt01>
2022-02-16 11:24:43 +0100

    -Training of DeLaN models on Panda simulations, data efficiency tests.

--- FE_panda_DELAN_model_estimation.py
+++ FE_panda_DELAN_model_estimation.py
@@ -2,49 +2,33 @@
 Script for training/testing of Deep Lagrangian Network of simulated data of a Panda robot.
 
 Author: Niccolò Turcato (niccolo.turcato@studenti.unipd.it)
-
 """
+from matplotlib import pyplot as plt
 
-# %%
-# Preamble 
-
-from torch._C import device
-
+import Utils
 import robust_fl_with_gps.Project_Utils as Project_FL_Utils
-import torch
-import torch.utils.data
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import pickle as pkl
-import time
-import argparse
 
 import argparse
 import torch
 import numpy as np
 import time
+import pickle as pkl
 
 import PyQt5
 
 import matplotlib as mp
 
+from deep_lagrangian_networks.DeLaN_model import DeepLagrangianNetwork
 from deep_lagrangian_networks.replay_memory import PyTorchReplayMemory
+from pytorchtools import EarlyStopping
 
 try:
     mp.use("Qt5Agg")
     mp.rc('text', usetex=True)
     # mp.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
     mp.rcParams['text.latex.preamble'] = r"\usepackage{bm} \usepackage{amsmath}"
-
 except ImportError:
     pass
-
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
-from deep_lagrangian_networks.DeLaN_model import DeepLagrangianNetwork
-
 data_path = ""
 training_file = ""
 test_file = ""
@@ -63,24 +47,32 @@ flg_save = 0
 parser = argparse.ArgumentParser('FE panda GIP estimator')
 parser.add_argument('-robot_name',
                     type=str,
-                    default='FE_panda_pybul',
+                    default='FE_panda_sim_',
                     help='Name of the robot.')
 parser.add_argument('-data_path',
                     type=str,
-                    default='./robust_fl_with_gps/Simulated_robots/Pybullet_sim/FE_panda/data/',
+                    default='./robust_fl_with_gps/Simulated_robots/SympyBotics_sim/FE_panda/',
                     help='Path to the folder containing training and test dasets.')
 parser.add_argument('-saving_path',
                     type=str,
-                    default='./robust_fl_with_gps/Results/GP_estimators/',
+                    default='./data/Results/DeLan/',
+                    help='Path to the destination folder for the generated files.')
+parser.add_argument('-model_saving_path',
+                    type=str,
+                    default='./data/',
                     help='Path to the destination folder for the generated files.')
 parser.add_argument('-training_file',
                     type=str,
-                    default='FE_panda_pybul_tr.pkl',
+                    default='FE_panda_sim_tr.pkl',
                     help='Name of the file containing the train dataset.')
 parser.add_argument('-test_file',
                     type=str,
-                    default='FE_panda_pybul_sum_of_sin_test.pkl',
+                    default='FE_panda_sim_test.pkl',
                     help='Name of the file containing the test dataset.')
+parser.add_argument('-noised_targets_file',
+                    type=str,
+                    default='FE_panda_sim_train_test_targets_noised.pkl',
+                    help='Name of the file containing the noised targets.')
 parser.add_argument('-flg_load',
                     type=bool,
                     default=False,
@@ -95,7 +87,7 @@ parser.add_argument('-flg_train',
                     help='Flag train. If True the model parameters are trained.')
 parser.add_argument('-batch_size',
                     type=int,
-                    default=500,
+                    default=512,
                     help='Batch size for the training procedure.')
 parser.add_argument('-shuffle',
                     type=bool,
@@ -103,11 +95,15 @@ parser.add_argument('-shuffle',
                     help='Shuffle data before training.')
 parser.add_argument('-flg_norm',
                     type=bool,
-                    default=True,
+                    default=False,
                     help='Normalize signal.')
+parser.add_argument('-flg_noise',
+                    type=bool,
+                    default=False,
+                    help='Set the device type')
 parser.add_argument('-N_epoch',
                     type=int,
-                    default=4000,
+                    default=5000,
                     help='Number of Epoch for the training procedure.')
 parser.add_argument('-N_epoch_print',
                     type=int,
@@ -127,15 +123,16 @@ parser.add_argument('-num_threads',
                     help='Number of computational threads.')
 parser.add_argument('-downsampling',
                     type=int,
-                    default=1,
+                    default=10,
                     help='Downsampling.')
+
 locals().update(vars(parser.parse_known_args()[0]))
 # %%
 # Set flags -- for debug
 flg_train = True
 #flg_train = False
 
-# flg_save = True
+flg_save = True
 
 flg_noise = True
 #flg_noise = False
@@ -160,7 +157,7 @@ test_path = data_path + test_file
 # Set robot params
 print('Setting robot parameters... ', end='')
 
-num_dof = 3
+num_dof = 7
 joint_index_list = range(0, num_dof)
 robot_structure = [0] * num_dof  # 0 = revolute, 1 = prismatic
 joint_names = [str(joint_index) for joint_index in range(1, num_dof + 1)]
@@ -210,8 +207,8 @@ if downsampling > 1:
     print('Done!')
 
 print("\n\n################################################")
-print("Characters:")
-print("# Training Samples = {0:05d}".format(int(train_qp.shape[0])))
+print("# Training Samples = {0:05d}".format(int(X_tr.shape[0])))
+print("# Test Samples = {0:05d}".format(int(X_test.shape[0])))
 print("")
 
 # Training Parameters:
@@ -225,166 +222,117 @@ hyper = {'n_width': 64,
          'activation': 'SoftPlus',
          'b_init': 1.e-4,
          'b_diag_init': 0.001,
-         'w_init': 'xavier_normal',
+         'w_init': 'orthogonal',
          'gain_hidden': np.sqrt(2.),
          'gain_output': 0.1,
          'n_minibatch': 512,
-         'learning_rate': 5.e-04,
+         'learning_rate': 50.e-04,
          'weight_decay': 1.e-5,
-         'max_epoch': 1}
+         'max_epoch': 30000,
+         'save_file': model_saving_path + path_suff + 'delan_panda3DOF_model.torch'}
 
-cuda = False  # Watch this
 
-delan_model = DeepLagrangianNetwork(n_dof, **hyper)
-delan_model = delan_model.cuda() if cuda else delan_model.cpu()
+# Splitting test-val dataset
+split = 10  # N_train/split samples to val and (Ntrain - N_train/split) to train
+val_size = int(X_tr.shape[0] / split)
+X_val = X_tr[X_tr.shape[0] - val_size:, :]
+Y_val = Y_tr[Y_tr.shape[0] - val_size:, :]
+X_tr = X_tr[:X_tr.shape[0] - val_size, :]
+Y_tr = Y_tr[:Y_tr.shape[0] - val_size, :]
 
-# Generate & Initialize the Optimizer:
-optimizer = torch.optim.Adam(delan_model.parameters(),
-                             lr=hyper["learning_rate"],
-                             weight_decay=hyper["weight_decay"],
-                             amsgrad=True)
+patience = int(hyper['max_epoch'] / 300)
 
-# Generate Replay Memory:
-mem_dim = ((n_dof,), (n_dof,), (n_dof,), (n_dof,))
-mem = PyTorchReplayMemory(train_qp.shape[0], hyper["n_minibatch"], mem_dim, cuda)
-mem.add_samples([train_qp, train_qv, train_qa, train_tau])
+early_stopping = EarlyStopping(patience=patience, verbose=False)
 
-# Start Training Loop:
-t0_start = time.perf_counter()
+hyper['save_file'] = model_saving_path + path_suff + 'delan_panda3DOF_model.torch'
 
-epoch_i = 0
-while epoch_i < hyper['max_epoch']:
-    l_mem_mean_inv_dyn, l_mem_var_inv_dyn = 0.0, 0.0
-    l_mem_mean_dEdt, l_mem_var_dEdt = 0.0, 0.0
-    l_mem, n_batches = 0.0, 0.0
+if flg_train:
+    delan_model = DeepLagrangianNetwork(num_dof, **hyper)
+    delan_model = delan_model.cuda(torch.device('cuda:0')) if flg_cuda else delan_model.cpu()
+    optimizer = torch.optim.Adam(delan_model.parameters(),
+                                 lr=hyper["learning_rate"],
+                                 weight_decay=hyper["weight_decay"],
+                                 amsgrad=True)
 
-    for q, qd, qdd, tau in mem:
-        t0_batch = time.perf_counter()
+    train_loss, val_loss = delan_model.train_model(X_tr, Y_tr, optimizer, save_model=flg_save,
+                                                   early_stopping=early_stopping, X_val=X_val, Y_val=Y_val)
 
-        # Reset gradients:
-        optimizer.zero_grad()
+elif flg_load:
+    state = torch.load(hyper['save_file'])
 
-        # Compute the Rigid Body Dynamics Model:
-        tau_hat, dEdt_hat = delan_model(q, qd, qdd)
+    delan_model = DeepLagrangianNetwork(num_dof, **state['hyper'])
+    delan_model.load_state_dict(state['state_dict'])
+    delan_model = delan_model.cuda(torch.device('cuda:0')) if flg_cuda else delan_model.cpu()
 
-        # Compute the loss of the Euler-Lagrange Differential Equation:
-        err_inv = torch.sum((tau_hat - tau) ** 2, dim=1)
-        l_mean_inv_dyn = torch.mean(err_inv)
-        l_var_inv_dyn = torch.var(err_inv)
+else:
+    raise RuntimeError('Aborting because no model training or loading was defined')
 
-        # Compute the loss of the Power Conservation:
-        dEdt = torch.matmul(qd.view(-1, n_dof, 1).transpose(1, 2), tau.view(-1, n_dof, 1)).view(-1)
-        err_dEdt = (dEdt_hat - dEdt) ** 2
-        l_mean_dEdt = torch.mean(err_dEdt)
-        l_var_dEdt = torch.var(err_dEdt)
+delan_model.cpu()
 
-        # Compute gradients & update the weights:
-        loss = l_mean_inv_dyn + l_mem_mean_dEdt
-        loss.backward()
-        optimizer.step()
 
-        # Update internal data:
-        n_batches += 1
-        l_mem += loss.item()
-        l_mem_mean_inv_dyn += l_mean_inv_dyn.item()
-        l_mem_var_inv_dyn += l_var_inv_dyn.item()
-        l_mem_mean_dEdt += l_mean_dEdt.item()
-        l_mem_var_dEdt += l_var_dEdt.item()
+if(flg_train):
+    plt.figure()
+    plt.title('Plot of train-val losses')
+    plt.plot(train_loss, color='b', label='train loss')
+    plt.plot(val_loss, color='g', label='val loss')
+    plt.legend()
+    plt.show()
 
-        t_batch = time.perf_counter() - t0_batch
+test_qp, test_qv, test_qa = Utils.unpack_dataset_joint_variables(X_test, num_dof)
 
-    # Update Epoch Loss & Computation Time:
-    l_mem_mean_inv_dyn /= float(n_batches)
-    l_mem_var_inv_dyn /= float(n_batches)
-    l_mem_mean_dEdt /= float(n_batches)
-    l_mem_var_dEdt /= float(n_batches)
-    l_mem /= float(n_batches)
-    epoch_i += 1
+delan_test_tau = delan_model.evaluate(X_test)
 
-    if epoch_i == 1 or np.mod(epoch_i, 100) == 0:
-        print("Epoch {0:05d}: ".format(epoch_i), end=" ")
-        print("Time = {0:05.1f}s".format(time.perf_counter() - t0_start), end=", ")
-        print("Loss = {0:.3e}".format(l_mem), end=", ")
-        print("Inv Dyn = {0:.3e} \u00B1 {1:.3e}".format(l_mem_mean_inv_dyn, 1.96 * np.sqrt(l_mem_var_inv_dyn)),
-              end=", ")
-        print("Power Con = {0:.3e} \u00B1 {1:.3e}".format(l_mem_mean_dEdt, 1.96 * np.sqrt(l_mem_var_dEdt)))
+q = torch.from_numpy(test_qp).float().to(delan_model.device)
+qd = torch.from_numpy(test_qv).float().to(delan_model.device)
+qdd = torch.from_numpy(test_qa).float().to(delan_model.device)
+zeros = torch.zeros_like(q).float().to(delan_model.device)
+# Computing torque decomposition
+with torch.no_grad():
+    delan_test_g = delan_model.inv_dyn(q, zeros, zeros).cpu().numpy().squeeze()
+    delan_test_c = delan_model.inv_dyn(q, qd, zeros).cpu().numpy().squeeze() - delan_test_g
+    delan_test_m = delan_model.inv_dyn(q, zeros, qdd).cpu().numpy().squeeze() - delan_test_g
 
-# Save the Model:
+X_tr = np.vstack((X_tr, X_val))
+Y_tr = np.vstack((Y_tr, Y_val))
 
-torch.save({"epoch": epoch_i,
-            "hyper": hyper,
-            "state_dict": delan_model.state_dict()},
-           "data/delan_model.torch")
+delan_tr_tau = delan_model.evaluate(X_tr)
+train_qp, train_qv, train_qa = Utils.unpack_dataset_joint_variables(X_tr, num_dof)
+# Convert NumPy samples to torch:
+q = torch.from_numpy(train_qp).float().to(delan_model.device)
+qd = torch.from_numpy(train_qv).float().to(delan_model.device)
+qdd = torch.from_numpy(train_qa).float().to(delan_model.device)
+zeros = torch.zeros_like(q).float().to(delan_model.device)
+# Computing torque decomposition
+with torch.no_grad():
+    delan_tr_g = delan_model.inv_dyn(q, zeros, zeros).cpu().numpy().squeeze()
+    delan_tr_c = delan_model.inv_dyn(q, qd, zeros).cpu().numpy().squeeze() - delan_tr_g
+    delan_tr_m = delan_model.inv_dyn(q, zeros, qdd).cpu().numpy().squeeze() - delan_tr_g
 
-print("\n################################################")
-print("Evaluating DeLaN:")
+#tr_estimates_saving_path = 'data/' + robot_name + 'orig_model_' + path_suff + 'DeLaN_train_estimates.pkl'
+tr_estimates_saving_path = 'data/' + robot_name + path_suff + 'DeLaN_train_estimates.pkl'
+test_estimates_saving_path = 'data/' + robot_name  + path_suff + 'DeLaN_test_estimates.pkl'
+#test_estimates_saving_path = 'data/' + robot_name + 'orig_model_' + path_suff + 'DeLaN_test_estimates.pkl'
 
-Y_tr_hat_list = [[] for i in range(num_dof)]
-Y_test_hat_list = [[] for i in range(num_dof)]
+pd_test_estimates = Utils.convert_predictions_to_dataset(
+    np.hstack([delan_test_tau, delan_test_m, delan_test_c, delan_test_g]),
+    ['tau_est', 'm_est', 'c_est', 'g_est'], range(num_dof))
 
-# Training estimates
-for i in range(train_qp.shape[0]):
-    with torch.no_grad():
-        # Convert NumPy samples to torch:
-        q = torch.from_numpy(train_qp[i]).float().view(1, -1)
-        qd = torch.from_numpy(train_qv[i]).float().view(1, -1)
-        qdd = torch.from_numpy(train_qa[i]).float().view(1, -1)
+pd_tr_estimates = Utils.convert_predictions_to_dataset(np.hstack([delan_tr_tau, delan_tr_m, delan_tr_c, delan_tr_g]),
+                                                       ['tau_est', 'm_est', 'c_est', 'g_est'], range(num_dof))
 
-        # Compute predicted torque:
-        out = delan_model(q, qd, qdd)
-        tau = out[0].cpu().numpy().squeeze()
-        for j in range(num_dof):
-            Y_tr_hat_list[j].append([tau[j]])
+Y_tr_noiseless_pd = Utils.convert_predictions_to_dataset(Y_tr, ['tau'], range(num_dof))
+Y_test_noiseless_pd = Utils.convert_predictions_to_dataset(Y_test, ['tau'], range(num_dof))
 
-# Test estimates
-for i in range(test_qp.shape[0]):
-    with torch.no_grad():
-        # Convert NumPy samples to torch:
-        q = torch.from_numpy(test_qp[i]).float().view(1, -1)
-        qd = torch.from_numpy(test_qv[i]).float().view(1, -1)
-        qdd = torch.from_numpy(test_qa[i]).float().view(1, -1)
-
-        # Compute predicted torque:
-        out = delan_model(q, qd, qdd)
-        tau = out[0].cpu().numpy().squeeze()
-        for j in range(num_dof):
-            Y_test_hat_list[j].append([tau[j]])
-
-for i in range(num_dof):
-    Y_tr_hat_list[i] = np.array(Y_tr_hat_list[i])
-    Y_test_hat_list[i] = np.array(Y_test_hat_list[i])
-
-norm_coeff = 1
-
-flg_norm=False
-
-# Print estimates and stats
-Y_tr_hat_pd, Y_test_hat_pd, Y_tr_pd, Y_test_pd, Y_tr_noiseless_pd, Y_test_noiseless_pd = Project_FL_Utils.get_pandas_obj(
-    output_tr=Y_tr,
-    output_test=Y_test,
-    noiseless_output_tr=Y_tr,
-    noiseless_output_test=Y_test,
-    Y_tr_hat_list=Y_tr_hat_list,
-    Y_test_hat_list=Y_test_hat_list,
-    flg_norm=flg_norm,
-    norm_coeff=norm_coeff,
-    joint_index_list=joint_index_list,
-    output_feature=output_feature,
-    noiseless_output_feature=output_feature)
-
-# get the erros stats
-Project_FL_Utils.get_stat_estimate(Y_tr_noiseless_pd, [Y_tr_hat_pd], joint_index_list, stat_name='nMSE',
+Project_FL_Utils.get_stat_estimate(Y_tr_noiseless_pd, [pd_tr_estimates], joint_index_list, stat_name='nMSE',
+                                   output_feature='tau')
+Project_FL_Utils.get_stat_estimate(Y_test_noiseless_pd, [pd_test_estimates], joint_index_list, stat_name='nMSE',
                                    output_feature=output_feature)
-Project_FL_Utils.get_stat_estimate(Y_test_noiseless_pd, [Y_test_hat_pd], joint_index_list, stat_name='nMSE',
-                                   output_feature=output_feature)
-# Project_Utils.get_stat_estimate(Y_test2_noiseless_pd, [Y_test2_hat_pd], joint_index_list, stat_name='nMSE', output_feature=output_feature, output_feature_noiseless=noiseless_output_feature)
-# print the estimates
-Project_FL_Utils.print_estimate(Y_tr_pd, [Y_tr_hat_pd], joint_index_list, flg_print_var=True,
-                                output_feature=output_feature, data_noiseless=Y_tr_noiseless_pd,
-                                noiseless_output_feature=output_feature)
-# plt.show()
-Project_FL_Utils.print_estimate(Y_test_pd, [Y_test_hat_pd], joint_index_list, flg_print_var=True,
-                                output_feature=output_feature)
-plt.show()
-# Project_Utils.print_estimate(Y_test2_pd, [Y_test2_hat_pd], joint_index_list, flg_print_var=True, output_feature=output_feature)
-# plt.show()
+
+
+if flg_save:
+    print("Saving estimates...")
+    pkl.dump(pd_tr_estimates, open(tr_estimates_saving_path, 'wb'))
+    pkl.dump(pd_test_estimates, open(test_estimates_saving_path, 'wb'))
+    print("Done!")
+
+print("\n################################################\n\n\n")
---
+++ data_eff_7dof_panda.py
@@ -0,0 +1,235 @@
+"""
+Script for Data efficiency evaluation on Deep Lagrangian Networks - 7dof data Panda robot
+
+Author: Niccolò Turcato (niccolo.turcato@studenti.unipd.it)
+"""
+import pickle as pkl
+import Utils
+import robust_fl_with_gps.Project_Utils as Project_FL_Utils
+
+import argparse
+import torch
+import numpy as np
+
+data_path = ""
+training_file = ""
+test_file = ""
+training_file_M = ""
+test_file_M = ""
+saving_path = ""
+robot_name = ""
+flg_norm = None
+flg_train = None
+shuffle = 0
+N_epoch_print = 0
+flg_save = 0
+noised_targets_file = ""
+
+path='robust_fl_with_gps/Simulated_robots/SympyBotics_sim/FE_panda/FE_panda_sim_tr.pkl'
+# %%
+# Set command line arguments
+parser = argparse.ArgumentParser('Data efficiency test on 2DOF data')
+parser.add_argument('-robot_name',
+                    type=str,
+                    default='FE_panda',
+                    help='Name of the robot.')
+parser.add_argument('-data_path',
+                    type=str,
+                    default='robust_fl_with_gps/Simulated_robots/SympyBotics_sim/FE_panda/',
+                    help='Path to the folder containing training and test dasets.')
+parser.add_argument('-saving_path',
+                    type=str,
+                    default='data_efficiency/',
+                    help='Path to the destination folder for the generated files.')
+parser.add_argument('-model_saving_path',
+                    type=str,
+                    default='data_efficiency/data/',
+                    help='Path to the destination folder for the generated files.')
+parser.add_argument('-training_file',
+                    type=str,
+                    default='FE_panda_sim_tr.pkl',
+                    help='Name of the file containing the train dataset.')
+parser.add_argument('-test_file',
+                    type=str,
+                    default='FE_panda_sim_test.pkl',
+                    help='Name of the file containing the test dataset.')
+parser.add_argument('-noised_targets_file',
+                    type=str,
+                    default='FE_panda_sim_train_test_targets_noised.pkl',
+                    help='Name of the file containing the noised targets.')
+parser.add_argument('-flg_load',
+                    type=bool,
+                    default=False,
+                    help='Flag load model. If True the model loaded from memory, otherwise they are computed.')
+parser.add_argument('-flg_save',
+                    type=bool,
+                    default=True,
+                    help='Flag save model. If true, the model parameters are saved in memory.')
+parser.add_argument('-flg_train',
+                    type=bool,
+                    default=True,
+                    help='Flag train. If True the model parameters are trained.')
+parser.add_argument('-flg_noise',
+                    type=bool,
+                    default=False,
+                    help='Set the device type')
+parser.add_argument('-batch_size',
+                    type=int,
+                    default=512,
+                    help='Batch size for the training procedure.')
+parser.add_argument('-shuffle',
+                    type=bool,
+                    default=True,
+                    help='Shuffle data before training.')
+parser.add_argument('-flg_norm',
+                    type=bool,
+                    default=False,
+                    help='Normalize signal.')
+parser.add_argument('-N_epoch',
+                    type=int,
+                    default=5000,
+                    help='Number of Epoch for the training procedure.')
+parser.add_argument('-N_epoch_print',
+                    type=int,
+                    default=1,
+                    help='Num epoch between two prints during training.')
+parser.add_argument('-flg_cuda',
+                    type=bool,
+                    default=False,
+                    help='Set the device type')
+parser.add_argument('-num_data_tr',
+                    type=int,
+                    default=None,
+                    help='Number of data to use in the training.')
+parser.add_argument('-num_threads',
+                    type=int,
+                    default=1,
+                    help='Number of computational threads.')
+parser.add_argument('-downsampling',
+                    type=int,
+                    default=10,
+                    help='Downsampling.')
+locals().update(vars(parser.parse_known_args()[0]))
+
+# %%
+# Set flags -- for debug
+flg_train = True
+#flg_train = False
+
+flg_save = True
+# flg_save = False
+
+flg_load = False
+#flg_load = True
+
+#flg_noise = True
+flg_noise = False
+
+
+flg_cuda = True
+# flg_cuda = True  # Watch this
+
+downsampling = 10
+num_threads = 4
+N_epoch = 500
+norm_coeff = 1
+dtype = torch.float64
+
+# Set the paths
+print('Setting paths... ', end='')
+
+path_suff = ''
+
+# Datasets loading paths
+tr_path = data_path + training_file
+test_path = data_path + test_file
+
+print('done!')
+
+# Set robot params
+print('Setting robot parameters... ', end='')
+
+num_dof = 7
+joint_index_list = range(0, num_dof)
+robot_structure = [0] * num_dof  # 0 = revolute, 1 = prismatic
+joint_names = [str(joint_index) for joint_index in range(1, num_dof + 1)]
+features_name_list = [str(i) for i in range(1, num_dof + 1)]
+output_feature = 'tau'
+
+print('done!')
+
+# Load datasets
+print('Loading datasets... ', end='')
+
+q_names = ['q_' + joint_name for joint_name in joint_names]
+dq_names = ['dq_' + joint_name for joint_name in joint_names]
+ddq_names = ['ddq_' + joint_name for joint_name in joint_names]
+input_features = q_names + dq_names + ddq_names
+pos_indices = range(0, num_dof)
+acc_indices = range(2 * num_dof, 3 * num_dof)
+input_features_joint_list = [input_features] * num_dof
+
+# Read the dataset:
+X_tr, Y_tr, active_dims_list, data_frame_tr = Project_FL_Utils.get_data_from_features(tr_path,
+                                                                                      input_features,
+                                                                                      input_features_joint_list,
+                                                                                      output_feature,
+                                                                                      num_dof)
+X_test, Y_test, _, data_frame_test = Project_FL_Utils.get_data_from_features(test_path,
+                                                                             input_features,
+                                                                             input_features_joint_list,
+                                                                             output_feature,
+                                                                             num_dof)
+
+if flg_noise:
+    noised_targets_file = data_path + noised_targets_file
+    Y_tr_noised, Y_test_noised = pkl.load(open(noised_targets_file, 'rb'))
+    Y_tr = Y_tr_noised
+    Y_test = Y_test_noised
+    path_suff += 'noised_'
+
+if downsampling > 1:
+    path_suff += "downsampling_" + str(downsampling)
+    print('## Downsampling training signals... ', end='')
+    X_tr = X_tr[::downsampling]
+    Y_tr = Y_tr[::downsampling]
+    print('Done!')
+
+# Training Parameters:
+print("\n################################################")
+print("Training Deep Lagrangian Networks (DeLaN):")
+
+# Construct Hyperparameters:
+# hyper = {'n_width': 64,
+#          'n_depth': 2,
+#          'diagonal_epsilon': 0.01,
+#          'activation': 'SoftPlus',
+#          'b_init': 1.e-4,
+#          'b_diag_init': 0.001,
+#          'w_init': 'xavier_normal',
+#          'gain_hidden': np.sqrt(2.),
+#          'gain_output': 0.1,
+#          'learning_rate': 5.e-04,
+#          'weight_decay': 1.e-5,
+#          'max_epoch': 250}
+
+hyper = {"n_width": 128, "n_depth": 2, "diagonal_epsilon": 0.01, "activation": "ReLu", "b_init": 0.0001,
+         "b_diag_init": 0.001, "w_init": "orthogonal", "gain_hidden": 1.4142135623730951, "gain_output": 0.1,
+         "n_minibatch": 512, "learning_rate": 0.01, "weight_decay": 1e-05, "max_epoch": 500}
+
+### DATA EFFICIENCY TEST
+
+# num_data_tr = 2400
+num_data_tr = X_tr.shape[0]
+print('TRAIN: {}'.format(num_data_tr))
+
+#CALL function
+test_results = Utils.data_efficiency_test(hyper, X_tr, Y_tr, X_test, Y_test, k_repetition=5)
+
+print(test_results)
+
+
+#efficiency_results = saving_path + 'orig_model_' + path_suff + 'data_efficiency_results_{}.pkl'.format(robot_name)
+efficiency_results = saving_path + path_suff + 'data_efficiency_results_{}.pkl'.format(robot_name)
+
+pkl.dump(test_results, open(efficiency_results, 'wb'))
