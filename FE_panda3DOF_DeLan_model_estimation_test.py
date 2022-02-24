9975aa66ba9443d007388f2719a4bb05dec8bdba
turcato_niccolo <Lup3tt01>
2022-02-24 11:05:09 +0100

    -Trained original DeLaN on 3DOF noised data.

--- FE_panda3DOF_DeLan_model_estimation_test.py
+++ FE_panda3DOF_DeLan_model_estimation_test.py
@@ -219,30 +219,30 @@ print("\n################################################")
 print("Training Deep Lagrangian Networks (DeLaN):")
 
 # Construct Hyperparameters:
-# hyper = {'n_width': 64,
-#          'n_depth': 2,
-#          'diagonal_epsilon': 0.01,
-#          'activation': 'SoftPlus',
-#          'b_init': 1.e-4,
-#          'b_diag_init': 0.001,
-#          'w_init': 'orthogonal',
-#          'gain_hidden': np.sqrt(2.),
-#          'gain_output': 0.1,
-#          'n_minibatch': 512,
-#          'learning_rate': 0.001,
-#          'weight_decay': 1.e-5,
-#          'max_epoch': 10000,
-#          'save_file': model_saving_path + path_suff + 'delan_panda3DOF_orig_model.torch'}
+hyper = {'n_width': 64,
+         'n_depth': 2,
+         'diagonal_epsilon': 0.01,
+         'activation': 'SoftPlus',
+         'b_init': 1.e-4,
+         'b_diag_init': 0.001,
+         'w_init': 'orthogonal',
+         'gain_hidden': np.sqrt(2.),
+         'gain_output': 0.1,
+         'n_minibatch': 512,
+         'learning_rate': 0.0005,
+         'weight_decay': 1.e-5,
+         'max_epoch': 10000,
+         'save_file': model_saving_path + path_suff + 'delan_panda3DOF_orig_model.torch'}
 
-hyper = {"n_width": 128, "n_depth": 2, "diagonal_epsilon": 0.01, "activation": "ReLu", "b_init": 0.0001,
-         "b_diag_init": 0.001, "w_init": "orthogonal", "gain_hidden": 1.4142135623730951, "gain_output": 0.1,
-         "n_minibatch": 512, "learning_rate": 0.01, "weight_decay": 1e-05, "max_epoch": 10000,
-         'save_file': model_saving_path + path_suff + 'delan_panda3DOF_new_model.torch'}
+# hyper = {"n_width": 128, "n_depth": 2, "diagonal_epsilon": 0.01, "activation": "ReLu", "b_init": 0.0001,
+#          "b_diag_init": 0.001, "w_init": "orthogonal", "gain_hidden": 1.4142135623730951, "gain_output": 0.1,
+#          "n_minibatch": 512, "learning_rate": 0.01, "weight_decay": 1e-05, "max_epoch": 10000,
+#          'save_file': model_saving_path + path_suff + 'delan_panda3DOF_new_model.torch'}
 
-#tr_estimates_saving_path = 'data/' + robot_name + 'orig_model_' + path_suff + 'DeLaN_train_estimates.pkl'
-tr_estimates_saving_path = 'data/' + robot_name + path_suff + 'DeLaN_new_model_train_estimates.pkl'
-test_estimates_saving_path = 'data/' + robot_name  + path_suff + 'DeLaN_new_model_test_estimates.pkl'
-#test_estimates_saving_path = 'data/' + robot_name + 'orig_model_' + path_suff + 'DeLaN_test_estimates.pkl'
+tr_estimates_saving_path = 'data/' + robot_name + 'orig_model_' + path_suff + 'DeLaN_train_estimates.pkl'
+# tr_estimates_saving_path = 'data/' + robot_name + path_suff + 'DeLaN_new_model_train_estimates.pkl'
+# test_estimates_saving_path = 'data/' + robot_name  + path_suff + 'DeLaN_new_model_test_estimates.pkl'
+test_estimates_saving_path = 'data/' + robot_name + 'orig_model_' + path_suff + 'DeLaN_test_estimates.pkl'
 
 # Splitting test-val dataset
 split = 10  # N_train/split samples to val and (Ntrain - N_train/split) to train
